88503e4c9e00291509ff380cf6216d2456c1decc
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-07-29 22:25:25 +0300

    config.el -> config.org: Add docs!!!

--- packages.el
+++ packages.el
@@ -5,6 +5,8 @@
 (package! org-bullets)
 (package! password-store)
 (package! go-mode)
+(package! fireplace)
+(package! org-alert)
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
